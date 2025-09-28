6e0906676c267cda893a2a3635569374b81c8582
Olovat <y.olovta@gmail.com>
2025-09-28 10:08:16 +0300

    reg&auth feature

--- src/config.php
+++ src/config.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 const COOKIE_TTL = 1 * 24 * 3600; // время жизни пакета ( в нашем случае пакет с залогининым пользователем) 1 день
 const COOKIE_PATH = '/';
 const COOKIE_SECURE = false; // сертификата нет 
-const COOKIE_HTTPONLY = false;
+const COOKIE_HTTPONLY = true; // чтобы токен был доступен из JS
 const COOKIE_SAMESITE = 'Strict'; // тестовый сайт нет переадресации
 ?>
